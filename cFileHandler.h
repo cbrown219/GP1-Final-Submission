9adc1c7bfccd40979fdfd30ea12c8c326ebe6a17
cbrown219 <cgbrown96@gmail.com>
2015-12-16 14:41:15 +0000

    Create cFileHandler.h

---
+++ cFileHandler.h
@@ -0,0 +1,31 @@
+/*
+=================
+cFileHandler.h
+- Header file for class definition - SPECIFICATION
+- Header file for the Sprite class
+=================
+*/
+#ifndef _CFILEHANDLER_H
+#define _CFILEHANDLER_H
+
+#include "GameConstants.h"
+#include <fstream>
+
+class cFileHandler
+{
+private:
+	string m_filename;
+	fstream theFile;
+
+public:
+	cFileHandler();
+	cFileHandler(string fileName);
+
+	int openFile(int mode);
+	void closeFile();
+	string readDataFromFile();
+	void writeDataToFile(string textLine);
+
+};
+
+#endif
