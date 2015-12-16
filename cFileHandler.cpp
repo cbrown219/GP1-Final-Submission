a3a9f1b9e92c00bc758e7f7268e8ff259e51136a
cbrown219 <cgbrown96@gmail.com>
2015-12-16 14:51:14 +0000

    Create cFileHandler.cpp

---
+++ cFileHandler.cpp
@@ -0,0 +1,54 @@
+/*
+=================
+- cTileMap.cpp
+- Header file for class definition - IMPLEMENTATION
+=================
+*/
+#include "cFileHandler.h"
+
+/*
+=================================================================
+Defualt Constructor
+=================================================================
+*/
+cFileHandler::cFileHandler()
+{
+	m_filename = "";
+}
+cFileHandler::cFileHandler(string fileName)
+{
+	m_filename = fileName;
+}
+
+int cFileHandler::openFile(int mode)
+{
+	theFile.open(m_filename, mode);
+	if (!theFile.is_open()) {
+		// Can not open file
+		return ERROR;
+	}
+	else {
+		return TRUE;
+	}
+}
+
+void cFileHandler::closeFile()
+{
+	theFile.close();
+}
+
+string cFileHandler::readDataFromFile()
+{
+	string lineFromFile;
+	string output;
+	while (getline(theFile, lineFromFile))
+	{
+		output += lineFromFile + ",";
+	}
+	return output;
+}
+
+void cFileHandler::writeDataToFile(string textLine)
+{
+	theFile << textLine;
+}
